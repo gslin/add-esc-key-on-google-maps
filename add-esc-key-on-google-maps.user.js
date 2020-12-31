9b982740b405b6eae1e4712c910b18d0a8f15baa
Gea-Suan Lin <gslin@gslin.org>
2020-12-31 09:24:46 +0800

    Import.

---
+++ add-esc-key-on-google-maps.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name        Add Esc key on Google Maps
+// @namespace   https://github.com/gslin/add-esc-key-on-google-maps
+// @match       https://www.google.com/maps*
+// @grant       none
+// @version     0.20201231.0
+// @author      Gea-Suan Lin <gslin@gslin.org>
+// @description Add Esc key on Google Maps
+// ==/UserScript==
+
+(() => {
+    'use strict';
+
+    window.addEventListener('keydown', ev => {
+        // Esc key
+        if (27 === ev.keyCode) {
+            let el = document.querySelector('a[guidedhelpid="clear_search"]');
+            if (!el) {
+                return;
+            }
+
+            el.click();
+        }
+    }, true);
+})();
