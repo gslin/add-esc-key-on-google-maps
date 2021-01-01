f44d85909d992b89bcafcf7fbe85c9d547b220dc
Gea-Suan Lin <gslin@gslin.org>
2021-01-01 12:19:25 +0800

    Add support for user page.

--- add-esc-key-on-google-maps.user.js
+++ add-esc-key-on-google-maps.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/gslin/add-esc-key-on-google-maps
 // @match       https://www.google.com/maps*
 // @grant       none
-// @version     0.20210101.0
+// @version     0.20210101.1
 // @author      Gea-Suan Lin <gslin@gslin.org>
 // @description Add Esc key on Google Maps
 // @license     MIT
@@ -15,8 +15,15 @@
     window.addEventListener('keydown', ev => {
         // Esc key
         if (27 === ev.keyCode) {
+            // User page (works only in traditional chinese environment).
+            let el = document.querySelector('button[aria-label="關閉"]');
+            if (el) {
+                el.click();
+                return;
+            }
+
             // Comment page (works only in traditional chinese environment).
-            let el = document.querySelector('button[aria-label="返回"]');
+            el = document.querySelector('button[aria-label="返回"]');
             if (el) {
                 el.click();
                 return;
