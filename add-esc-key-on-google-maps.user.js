343b672cbcab0277770d9799f31675de5380a5c0
Gea-Suan Lin <gslin@gslin.org>
2021-01-01 15:20:57 +0800

    Add visibility code.

--- add-esc-key-on-google-maps.user.js
+++ add-esc-key-on-google-maps.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/gslin/add-esc-key-on-google-maps
 // @match       https://www.google.com/maps*
 // @grant       none
-// @version     0.20210101.1
+// @version     0.20210101.2
 // @author      Gea-Suan Lin <gslin@gslin.org>
 // @description Add Esc key on Google Maps for better UX.
 // @license     MIT
@@ -17,21 +17,21 @@
         if (27 === ev.keyCode) {
             // User page (works only in traditional chinese environment).
             let el = document.querySelector('button[aria-label="關閉"]');
-            if (el) {
+            if (el && el.offsetHeight > 0 && el.offsetWidth > 0) {
                 el.click();
                 return;
             }
 
             // Comment page (works only in traditional chinese environment).
             el = document.querySelector('button[aria-label="返回"]');
-            if (el) {
+            if (el && el.offsetHeight > 0 && el.offsetWidth > 0) {
                 el.click();
                 return;
             }
 
             // Store page.
             el = document.querySelector('a[guidedhelpid="clear_search"]');
-            if (el) {
+            if (el && el.offsetHeight > 0 && el.offsetWidth > 0) {
                 el.click();
                 return;
             }
