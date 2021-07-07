2cae967f7a5e9baece91d5954af611faa3f59c53
Gea-Suan Lin <gslin@gslin.org>
2021-07-07 11:10:27 +0800

    Support "Close" aria.

--- add-esc-key-on-google-maps.user.js
+++ add-esc-key-on-google-maps.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/gslin/add-esc-key-on-google-maps
 // @match       https://www.google.com/maps*
 // @grant       none
-// @version     0.20210707.0
+// @version     0.20210707.1
 // @author      Gea-Suan Lin <gslin@gslin.org>
 // @description Add Esc key on Google Maps for better UX.
 // @license     MIT
@@ -15,14 +15,14 @@
     window.addEventListener('keydown', ev => {
         // Esc key
         if (27 === ev.keyCode) {
-            // User page (works only in traditional chinese environment).
-            let el = document.querySelector('button[aria-label="關閉"]');
+            // User page.
+            let el = document.querySelector('button[aria-label="Close"], button[aria-label="關閉"]');
             if (el && el.offsetHeight > 0 && el.offsetWidth > 0) {
                 el.click();
                 return;
             }
 
-            // Comment page (works only in traditional chinese environment).
+            // Comment page.
             el = document.querySelector('button[aria-label="Back"], button[aria-label="返回"]');
             if (el && el.offsetHeight > 0 && el.offsetWidth > 0) {
                 el.click();
