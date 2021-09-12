4dd7f248112af4bfba8d3c51769e0d770e025d6a
Gea-Suan Lin <gslin@gslin.org>
2021-09-12 14:09:22 +0800

    Refactor to make maintenance easier.

--- add-esc-key-on-google-maps.user.js
+++ add-esc-key-on-google-maps.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/gslin/add-esc-key-on-google-maps
 // @match       https://www.google.com/maps*
 // @grant       none
-// @version     0.20210707.1
+// @version     0.20210912.0
 // @author      Gea-Suan Lin <gslin@gslin.org>
 // @description Add Esc key on Google Maps for better UX.
 // @license     MIT
@@ -13,28 +13,30 @@
     'use strict';
 
     window.addEventListener('keydown', ev => {
-        // Esc key
-        if (27 === ev.keyCode) {
-            // User page.
-            let el = document.querySelector('button[aria-label="Close"], button[aria-label="關閉"]');
-            if (el && el.offsetHeight > 0 && el.offsetWidth > 0) {
-                el.click();
-                return;
-            }
+        // Esc only
+        if (27 !== ev.keyCode) {
+            return;
+        }
 
-            // Comment page.
-            el = document.querySelector('button[aria-label="Back"], button[aria-label="返回"]');
-            if (el && el.offsetHeight > 0 && el.offsetWidth > 0) {
-                el.click();
-                return;
-            }
+        // User page.
+        let el = document.querySelector('button[aria-label="Close"], button[aria-label="關閉"]');
+        if (el && el.offsetHeight > 0 && el.offsetWidth > 0) {
+            el.click();
+            return;
+        }
 
-            // Store page.
-            el = document.querySelector('a[guidedhelpid="clear_search"]');
-            if (el && el.offsetHeight > 0 && el.offsetWidth > 0) {
-                el.click();
-                return;
-            }
+        // Comment page.
+        el = document.querySelector('button[aria-label="Back"], button[aria-label="返回"]');
+        if (el && el.offsetHeight > 0 && el.offsetWidth > 0) {
+            el.click();
+            return;
+        }
+
+        // Store page.
+        el = document.querySelector('a[guidedhelpid="clear_search"]');
+        if (el && el.offsetHeight > 0 && el.offsetWidth > 0) {
+            el.click();
+            return;
         }
     }, true);
 })();
